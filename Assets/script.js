92365edaf42d05365c09071f5d758fd418bac0b2
Q118 <shelbyfish91@gmail.com>
2020-06-22 19:54:01 -0700

    displays temperature of user inputted city

--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,39 @@
 /** @format */
 
 //initial array of city buttons
-var cities = [];
-userCity = $();
-var apiKey = "cb2ced122314e011f2e654fe37600669";
+//var cities = [];
+//userCity = $();
+
 
 $("#search-button").on("click", function (event) {
 	event.preventDefault();
-
+    var apiKey = "cb2ced122314e011f2e654fe37600669";
 	var city = $("#city-input").val();
 	console.log(city);
 
 	var queryURL =
 		"https://api.openweathermap.org/data/2.5/weather?q=" +
-		city +
+		city + "&units=imperial" +
 		"&appid=" +
         apiKey;
         
         var a = $("<button>");
         a.text(city);
         $("#button-container").append(a);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    })
+    .then(function (response) {
+        console.log(response.name);
+        var info = $("<h2>");
+        info.text("Temperature: " + response.main.temp + " Fahrenheit");
+        $("#info-container").append(info);
+
+    })
 });
 
 
-//function renderButtons() {};
+
   
