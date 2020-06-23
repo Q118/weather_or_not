fa54cfeda3aca04e9d6e45fee32522fc2aeee861
Q118 <shelbyfish91@gmail.com>
2020-06-23 10:00:45 -0700

    wrapping with jquery

---
+++ Assets/fiveday.js
@@ -0,0 +1,26 @@
+//need to dump the data on to the page
+//then put on page
+//look at activity 6.9 clickJSON
+//and bounjanboora
+    //how can we use approach for our 5day
+        /* 1. create some static html
+            2. dump data for 5day forecast for tulsa, OK
+https://openweathermap.org/forecast5
+
+
+
+
+            3. ALert city user has input
+            4. dump data for city user entered
+            5.dynamically render html for the forecast
+            6. test various cities and deal with any bugs
+
+should i go back and have functions be seperate then call them on button click
+
+*add aria label for screen readers
+<div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+  <div class="input-group-append">
+    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+  </div>
+</div>
--- Assets/script.js
+++ Assets/script.js
@@ -1,39 +1,36 @@
 /** @format */
+$(function () {
+	//initial array of city buttons
+	//var cities = [];
+	//userCity = $();
 
-//initial array of city buttons
-//var cities = [];
-//userCity = $();
+	$("#search-button").on("click", function (event) {
+		event.preventDefault();
+		var apiKey = "cb2ced122314e011f2e654fe37600669";
+		var city = $("#city-input").val();
+		console.log(city);
 
-
-$("#search-button").on("click", function (event) {
-	event.preventDefault();
-    var apiKey = "cb2ced122314e011f2e654fe37600669";
-	var city = $("#city-input").val();
-	console.log(city);
-
-	var queryURL =
-		"https://api.openweathermap.org/data/2.5/weather?q=" +
-		city + "&units=imperial" +
-		"&appid=" +
-        apiKey;
-        
-        var a = $("<button>");
-        a.text(city);
-        $("#button-container").append(a);
-
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    })
-    .then(function (response) {
-        console.log(response.name);
-        var info = $("<h2>");
-        info.text("Temperature: " + response.main.temp + " Fahrenheit");
-        $("#info-container").append(info);
-
-    })
-});
-
-
-
+		var queryURL =
+			"https://api.openweathermap.org/data/2.5/weather?q=" +
+			city +
+			"&units=imperial" +
+			"&appid=" +
+			apiKey;
   
+		var a = $("<button>");
+		a.text(city);
+		$("#button-container").append(a);
+
+		$.ajax({
+			url: queryURL,
+			method: "GET",
+		}).then(function (response) {
+			console.log(response.name);
+			var info = $("<h2>");
+			info.text("Temperature: " + response.main.temp + " Fahrenheit");
+            $("#info-container").append(info);
+            //dump 5-day forecast
+            $("#forecast").html(JSON.stringify(response));
+		});
+	});
+});
