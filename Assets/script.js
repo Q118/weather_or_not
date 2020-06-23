543e4ae7ecbfb0b4f8fc1a8b159622a825b39639
Q118 <shelbyfish91@gmail.com>
2020-06-23 16:27:25 -0700

    adjusting grid display to hold the info

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,8 @@
 /** @format */
 $(function () {
 	//code only to run once html is loaded
-	//initial array of city buttons
 	//listen for clicks on search button
 	//then we read value of input element
-	//aler the value
 
 	$("#search-button").on("click", function (event) {
 		event.preventDefault();
@@ -34,25 +32,24 @@ $(function () {
 
 			var forecastEl = $("#forecast");
 			var forecastDays = [];
-			forecastDays.push(response.list[0]); //go until 32
+			//display one day at a time
+			forecastDays.push(response.list[0]); 
 			forecastDays.push(response.list[8]);
 			forecastDays.push(response.list[16]);
 			forecastDays.push(response.list[24]);
 			forecastDays.push(response.list[32]);
-			//forecastEl.html(
-			//	"<pre>" + JSON.stringify(forecastDays, null, 2) + "</pre>");
-			//null adds a function to not modifyand the 2 expresses the indentation
+		
 		
 			//create forecast-day div
 			for (var i=0; i< forecastDays.length; i ++) {
 				console.log(forecastDays[i]);
 			var forecastContainer = $("<div>").addClass("forecast-day");
 			//create elements for date, icon, temp, and humidity
-			var dateEl = $("<div>").text("date");
+			var dateEl = $("<div>").text(forecastDays[i].dt_txt);
 			//replace date with forecastDays[i].dt or with moment
 			//prob need to create an image
 			var iconEl = $("<div>").text("icon" + forecastDays[i].weather[0].icon);
-			var tempEl =$("<div>").text("temp" + forecastDays[i].main.temp + "degrees farenheight");
+			var tempEl =$("<div>").text("temp" + forecastDays[i].main.temp + "°F");
 			var humidityEl = $("<div>").text("humididty info");
 			//add data element to forecast-day div
 			forecastContainer.append(dateEl, iconEl, tempEl, humidityEl);
@@ -63,10 +60,10 @@ $(function () {
 		});
 	});
 
-	function handleSearch(event) {
+	//function handleSearch(event) {
 		//	event.preventDefault();
 		//	var city = $("#city-input").val();
-	}
+	//}
 });
 
 //get reference to element for showing the data
